731b42f92bcbafdd7141f8989eca09a0e835ff08
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@yahoo.com>
2020-10-01 13:58:33 +0300

    Codes were added.
    -WIP-

---
+++ MyATM.h
@@ -0,0 +1,22 @@
+#ifndef MYATM_H
+#define MYATM_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+#include <errno.h>
+
+#define CLEAR system("CLS");
+
+void UIScreen();
+void ATMBalance();
+void WithCreditCard();
+void WithoutCreditCard();
+
+#endif // !MYATM_H
+
+
+
