f5958f842f40d20ff04f87c05c614d4111990b79
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@yahoo.com>
2021-02-27 02:34:44 +0300

    Update on 27.02.2021
    In this update,
    - The functions, that were used in the ATM-with-Cpp repository, moved here.

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
-#include "MyATM.h"
-/*
-	- It is a simulated ATM program. It was created for learning purposes.
-	- We will save balances in .txt files.
-*/
+#include "ATM.h"
 
 
-int main() {
+int main()
+{
 
 
-UIScreen();
-	
+
+	ui_screen();
 
 
+
+
+	return 0;
 }
-
-
-	
-	
-	
-
-
